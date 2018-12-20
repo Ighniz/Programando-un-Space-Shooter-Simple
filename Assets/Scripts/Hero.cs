3400f4ef107fdfd1bcb4dcadc27e4ec5c6793fc2
Miguel Vedoya <miguelvedoya@gmail.com>
2018-12-20 09:23:12 -0300

    EJERCITACIÓN: Movimiento en todas las direcciones.

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -15,6 +15,18 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
     }
 }
