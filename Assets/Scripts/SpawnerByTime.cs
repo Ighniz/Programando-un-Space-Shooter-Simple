9b7f6c56fe30589af3b0d0995a749b012c514193
Miguel Vedoya <miguelvedoya@gmail.com>
2019-01-02 09:28:23 -0300

    Agregamos el script "SpawnerByTime".

---
+++ Assets/Scripts/SpawnerByTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerByTime : MonoBehaviour
+{
+    public float timeToSpawn;
+    public float currentSeconds;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        currentSeconds += Time.deltaTime;
+    }
+}
