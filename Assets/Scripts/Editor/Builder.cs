84d97c5b4cc2f28e2ef2d81b89de52286dd45988
Ighniz <miguelvedoya@gmail.com>
2020-03-06 23:09:17 -0300

    Added Builder script.

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    static public void PerformBuild()
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        foreach (var argument in args)
+        {
+            Debug.Log(argument);
+        }
+    }
+}
